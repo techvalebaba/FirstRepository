c362a215493c1f051c9e6e4edbd864f9909461a5
TechValeBaba <techvalebaba@Chetans-MacBook-Air.local>
2023-04-09 14:11:39 +0530

    h

--- PrintNNaturalNumber.java
+++ PrintNNaturalNumber.java
@@ -3,16 +3,12 @@ import java.util.Scanner;
 public class PrintNNaturalNumber {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Please Enter any number: ");
         int n = sc.nextInt();
-
         for(int i = 1; i <= n; i++)
         {
             System.out.println("The value of i: "+i);
         }
-
     }
 }
